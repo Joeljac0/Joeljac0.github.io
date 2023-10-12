9a6521da7b1c52bfd162b9d978459229b5fb6e6b
Joeljac0 <jacobsson.joel5@gmail.com>
2023-10-12 14:17:17 +0200

    crazy

---
+++ my 2/scr.js
@@ -0,0 +1,21 @@
+
+let name = 'Jool' //String Literal
+let age = 30 //Number Literal
+let isApproved = true //Boolean Literal (True or False)
+let firstName = undefined //Undefined
+let selectedColor = null //Om inget svar på fråga
+
+
+
+let person = {
+    name: 'Mosh',
+    age: 30
+}
+
+person.name = 'John'
+
+//console.log(person.name)
+
+let selectedcolors = ['red', 'blue']
+selectedcolors[2] = 1
+console.log(selectedcolors.length)
