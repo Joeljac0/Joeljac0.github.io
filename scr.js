d95cc9dced27a892723317df3997049e4b139ada
Joeljac0 <jacobsson.joel5@gmail.com>
2024-02-29 12:47:00 +0100

    yup
    yup

--- scr.js
+++ scr.js
@@ -1,68 +1,64 @@
-document.getElementById("imgClickAndChange").src = "cat.jpg";
+document.getElementById("imgClickAndChange").src = "cat.jpg"
 
 let x = 1
 
-function changeImage() {
-    if (x == 1){
-        document.getElementById("imgClickAndChange").src = "image.png";
-        x = 2
-    } 
-    else if (x == 2){
-    document.getElementById("imgClickAndChange").src = "joool.jpg";
-    x = 3
-    }
-    else if (x == 3){
-    document.getElementById("imgClickAndChange").src = "win.jpg";
-    x = 4
-    }
-    else if (x == 4){
-    document.getElementById("imgClickAndChange").src = "cat.jpg";
-    x = 1
-    }
+function changeImage() {    
+    switch (x) {
+        case 1:
+            document.getElementById("imgClickAndChange").src = "image.png"
+            x = 2
+            break
+        case 2:
+            document.getElementById("imgClickAndChange").src = "joool.jpg"
+            x = 3
+            break
+        case 3: 
+            document.getElementById("imgClickAndChange").src = "win.jpg"
+            x = 4
+            break
+        case 4:     
+            document.getElementById("imgClickAndChange").src = "cat.jpg"
+            x = 1
+            break
+    }  
 }
 
 /*function balls() {
     setTimeout(function () {
-        document.getElementById("head").style.backgroundColor = "white";
+        document.getElementById("body").style.backgroundColor = "white"
         setTimeout(function () {
-            document.getElementById("head").style.backgroundColor = "black";
-            balls(); // Repeat the function
-        }, 1000);
-    }, 1000);
+            document.getElementById("body").style.backgroundColor = "black"
+            balls() // Repeat the function
+        }, 1000)
+    }, 1000)
 }
 
 // Call the function to start the infinite repetition
-balls();*/
+balls()*/
 
 document.getElementById("yes").innerText = ""
 
 let t = 1
 let b = 1
 
+function balls() {
+    document.getElementById("body").style.backgroundColor = "white"
+    document.getElementById("body").style.backgroundColor = "black"
+}
+
 function Normal() {
     if (t == 1){
-        b = 1
         document.getElementById("yes").innerText = "CLICK PICTURE!!"
-        function balls() {
-            setTimeout(function () {
-                document.getElementById("head").style.backgroundColor = "white"
-                setTimeout(function () {
-                    document.getElementById("head").style.backgroundColor = "black"
-                    balls() // Repeat the function
-                }, 1000)
-            }, 1000)
-        }
-        
-        // Call the function to start the infinite repetition
-        balls()
-
+        //setInterval(balls(), 1000)
+       
         t = 2
     }
     else if (t == 2){
         b = 2
         document.getElementById("yes").innerText = ""
-        document.getElementById("head").style.backgroundColor = "purple"
-
+        document.getElementById("body").style.backgroundColor = "purple"
+        
         t = 1
+
     }
 }
